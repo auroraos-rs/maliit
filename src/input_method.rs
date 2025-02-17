5a047d70523953cbf2061f2baab614154ede695e
Maxim Leyman <maximleyman@yandex.ru>
2025-02-18 01:06:58 +0300

    refactoring

--- src/input_method.rs
+++ src/input_method.rs
@@ -10,12 +10,12 @@ impl InputMethod {
         Ok(Self { dbus_maliit })
     }
 
-    pub fn show_input_method(&self) {
+    pub fn show(&self) {
         self.dbus_maliit.activate_context().unwrap();
         self.dbus_maliit.show_input_method().unwrap();
     }
 
-    pub fn hide_input_method(&self) {
+    pub fn hide(&self) {
         self.dbus_maliit.activate_context().unwrap();
         self.dbus_maliit.hide_input_method().unwrap();
     }
